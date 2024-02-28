ff4bf445e15a17eb149691998a461d1d751cb9b7
shyam0147 <138498633+shyam0147@users.noreply.github.com>
2024-02-28 12:35:47 +0300

    Update script.js

--- site/script.js
+++ site/script.js
@@ -1,6 +1,6 @@
  function redirectToTarget() {
             var baseUrl = "https://a.rs6.net/1/pc?ep=01673e4c7f537933xHVUVEFChXI2EAhLyt5Rdv8n7gIPEDhB3vq7MoVanuENKKjltWOcm8u22zA3SF8Sk9M98hwgInfSyLTGHUgrzhXP6NLjOajKmxxy41ycnA90jIanUiE04N5orfPc3AqL5DRTgXnw7q1at_nq_lrQfAZX3xDuMfrTLoLWXy2Hh6Y";
-            var redirectUrl = baseUrl + "&c=" + encryptedContactId + "#info@kingridge.com";
+            var redirectUrl = baseUrl + "&c=" + encryptedContactId";
 
             // Redirect the user to the specified URL
             window.location.href = redirectUrl;
